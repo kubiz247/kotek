3fe89de8d1b3b42afd0661ebb6155b0f8487a857
Jakub <samonek2006@gmail.com>
2024-11-06 09:08:41 +0100

    tego typu

---
+++ kotek/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kotek
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kotek/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace kotek
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Kotek.Source = "https://cataas.com/cat?";
+        }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            Kotek.Source = "https://cataas.com/cat/cute/says/" + Kotek2.Text;
+        }
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            Kotek.Source = "https://cataas.com/cat/gif";
+        }
+        private void OnCounterClicked4(object sender, EventArgs e)
+        {
+            Kotek.Source = "https://cataas.com/cat/cute/says/" + Kotek3.Text;
+        }
+    }
+}
+
